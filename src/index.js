9902f3e7ecd4d253d00ff986248be81ab06eb10c
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-03-28 10:31:55 +0100

    joke

--- src/index.js
+++ src/index.js
@@ -1,5 +1,68 @@
-import data from './data.js';
+import getJoke from './api.js';
+
+const dom = {
+    jokeContainer: document.querySelector('.container'),
+    jokeButton: document.querySelector('.btn'),
+    jokeText: document.getElementById('joke-text'),
+};
+
+// Import the getJoke function from api.js and use it to fetch a joke
+const fetchJoke = async () => {
+    const joke = await getJoke();
+    if (joke) {
+        console.log(joke);
+        dom.jokeText.innerText = `${joke.setup} ${joke.delivery}`;
+    } else {
+        console.log('No joke found');
+    }
+  
+};
+
+dom.jokeButton.addEventListener('click', () => {
+    fetchJoke();
+});
 
 
-const now = new Date('2023-08-05 13:45:00');
-console.log(now);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import getJoke from './api.js';
+
+const dom = {
+    jokeContainer: document.querySelector('.container'),
+    jokeButton: document.querySelector('.btn'),
+};
+
+// Import the getJoke function from api.js and use it to fetch a joke
+const fetchJoke = async () => {
+    const joke = await getJoke();
+    if (joke) {
+        console.log(joke);
+    } else {
+        console.log('No joke found');
+    }
+};
+
+dom.jokeButton.addEventListener('click',() => {
+    
+    fetchJoke();
+    console.log('Button clicked!')
+})*/
