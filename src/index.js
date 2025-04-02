747aba0af923e7abda53c3f5062d96a3c0550e2d
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-04-02 21:55:45 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,68 +1,68 @@
-import getJoke from './api.js';
+import getJoke from '../api.js';
 
 const dom = {
-    jokeContainer: document.querySelector('.container'),
-    jokeButton: document.querySelector('.btn'),
-    jokeText: document.getElementById('joke-text'),
-};
+    container: document.getElementById('container'),
+    btn: document.getElementById('get-joke'),
+    joke: null,
+    category: null,
+    tags: null
+}
+ 
 
-// Import the getJoke function from api.js and use it to fetch a joke
-const fetchJoke = async () => {
-    const joke = await getJoke();
-    if (joke) {
-        console.log(joke);
-        dom.jokeText.innerText = `${joke.setup} ${joke.delivery}`;
-    } else {
-        console.log('No joke found');
+const createTags = (tags) => {
+    const currentTags = Object.values(tags).some(tag => tag);
+    if (!currentTags) {
+        if (dom.tags) {
+            dom.tags.innerHTML = '';
+        }
+        return;
     }
-  
-};
+    const filtered = Object.keys(tags).filter(flag => tags[flag]);
 
-dom.jokeButton.addEventListener('click', () => {
-    fetchJoke();
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import getJoke from './api.js';
-
-const dom = {
-    jokeContainer: document.querySelector('.container'),
-    jokeButton: document.querySelector('.btn'),
-};
-
-// Import the getJoke function from api.js and use it to fetch a joke
-const fetchJoke = async () => {
-    const joke = await getJoke();
-    if (joke) {
-        console.log(joke);
-    } else {
-        console.log('No joke found');
+    if (!dom.tags) {
+        dom.tags = document.createElement('div');
+        dom.tags.setAttribute('id', 'tags');
+        dom.container.append(dom.tags);
     }
-};
 
-dom.jokeButton.addEventListener('click',() => {
-    
-    fetchJoke();
-    console.log('Button clicked!')
-})*/
+    dom.tags.innerHTML = '';
+
+    filtered.forEach(tag => {
+        const tagBox = document.createElement('span');
+        tagBox.innerText = tag;
+        tagBox.setAttribute('id', 'joke');
+        dom.tags.append(tagBox);
+    })
+}
+
+const createCategory = (category) => {
+    if (!dom.category) {
+        dom.category = document.createElement('div');
+        dom.container.append(dom.category);
+        dom.category.setAttribute('id', 'category');
+    }
+
+    dom.category.innerText = 'Category: ' + category;
+}
+
+const joke = async () => {
+    const response = await getJoke();
+    createCategory(response.category);
+    createTags(response.flags);
+    let jokeText = '';
+    if (response.joke) {
+        jokeText += `${response.joke}`;
+    } else {
+        jokeText += `${response.setup}\n\n${response.delivery}`;
+    }
+
+    if (!dom.joke) {
+        dom.joke = document.createElement('div');
+        dom.container.append(dom.joke);
+        dom.joke.setAttribute('id', 'joke');
+    }
+
+    dom.joke.innerText = jokeText;
+}
+
+dom.btn.addEventListener('click', joke);
